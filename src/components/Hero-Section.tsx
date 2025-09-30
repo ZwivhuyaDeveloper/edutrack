6384e09e92a0e35b29bcb663aeeb38bb538ca9f8
cyclespace <stackecommerce@gmail.com>
2025-10-01 00:06:40 +0200

    Heor update

--- src/components/Hero-Section.tsx
+++ src/components/Hero-Section.tsx
@@ -26,7 +26,7 @@ function HeroSection() {
                 />
               </div>
             ),
-            className: "md:col-span-1 lg:col-span-1 h-[20rem] sm:h-[20rem] md:h-[15rem] lg:h-[10rem]",
+            className: "md:col-span-1 lg:col-span-1 hidden lg:block md:block",
         },
         {
             header: (
@@ -41,7 +41,7 @@ function HeroSection() {
                 />
               </div>
             ),
-            className: "md:col-span-2 lg:col-span-2 hidden lg:block md:block",
+            className: "md:col-span-2 lg:col-span-2 h-[20rem] sm:h-[20rem] md:h-[15rem] lg:h-[10rem]",
         },
         {
             header: (
